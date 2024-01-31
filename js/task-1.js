6ddfb448ce133c604749fef0cd696f39589368e7
Chinwilla <bogdandrozd123@gmail.com>
2024-01-31 14:34:06 +0000

    Update

---
+++ js/task-1.js
@@ -0,0 +1,30 @@
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+
+// const result = makePizza();
+// const pointer = makePizza;
+
+// console.log(makePizza)
+// console.log(makePizza("AbraKad"))
+
+//--------------------------------------------------------------------------------
+
+// function greet(name) {
+//   console.log(`Welcome ${name}!`);
+// }
+
+// function notify(name) {
+//   console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+// }
+
+// function registerGuest(name, callback) {
+//   console.log(`Registering ${name}!`);
+//   callback(name);
+// }
+
+// registerGuest("Mango", greet); // "Registering Mango!"
+// 															 // "Welcome Mango!"
+
+// registerGuest("Mango", notify); // "Registering Mango!"
+// 																// "Dear Mango, your room will be ready in 30 minutes"
