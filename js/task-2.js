534a83869dc8144c5e2d71a55dd7b649f0636abf
Chinwilla <bogdandrozd123@gmail.com>
2024-02-07 15:38:57 +0000

    Upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,95 @@
-function changeEven(numbers, value) {
-    let updatedArray = [];
-    for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] % 2 === 0) {
-      updatedArray.push(numbers[i] + value)
-    } else {
-        updatedArray.push(numbers[i])
-    }
-    }
-    return updatedArray;
-}
+
+
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+
+
+//  [----------------------------------------------------]
+
+  Game Speed: 4x
+  How many players: 5v5
+  Gold usage: Forbidden and off by default
+  Peace period: Undefined
+
+  Match start date: 
+  Match start time: 
+  War Start date: 
+  War start time: 
+  Seasonal units - 
+  Officers - 
+  Missiles - 
+  Stealth - 
+  Navy - 
+  Aircraft / Navy on water bodies near enemy territory: Not allowed
+  
+  During Peace period
+  Naval Peeking: Not allowed 
+  Flyovers: Not Allowed 
+  UAVs: Not allowed
+  Use of intel: Not allowed
+
+  During War Time
+  Other Players Right of Way: Not Allowed
+  Atack from players out of the coalitions: Not Allowed
+
+  In this game.The land is predefined. There are 2 divisions:
+  1.Europe +africa +north America <------- to define
+  2.Asia+middle east+South america <------- to define
+  Each group can not expand in the other group's land
+
+//  [----------------------------------------------------]
