f050cd5f298264826bed4aec7a697788818f7ff2
Chinwilla <bogdandrozd123@gmail.com>
2024-02-08 22:09:58 +0000

    gg

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,3 @@
-
-
-
 const allUsers = [
   {
     name: "Moore Hensley",
@@ -57,39 +54,3 @@ console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
 // ]
 
 console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
-
-
-//  [----------------------------------------------------]
-
-  Game Speed: 4x
-  How many players: 5v5
-  Gold usage: Forbidden and off by default
-  Peace period: Undefined
-
-  Match start date: 
-  Match start time: 
-  War Start date: 
-  War start time: 
-  Seasonal units - 
-  Officers - 
-  Missiles - 
-  Stealth - 
-  Navy - 
-  Aircraft / Navy on water bodies near enemy territory: Not allowed
-  
-  During Peace period
-  Naval Peeking: Not allowed 
-  Flyovers: Not Allowed 
-  UAVs: Not allowed
-  Use of intel: Not allowed
-
-  During War Time
-  Other Players Right of Way: Not Allowed
-  Atack from players out of the coalitions: Not Allowed
-
-  In this game.The land is predefined. There are 2 divisions:
-  1.Europe +africa +north America <------- to define
-  2.Asia+middle east+South america <------- to define
-  Each group can not expand in the other group's land
-
-//  [----------------------------------------------------]
